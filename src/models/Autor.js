f8c5aee8ee0f606ec5a557cf451e2fd8e40dedad
ikerglopez <ikergarcialopez01@gmail.com>
2025-08-03 19:27:06 +0200

    Libro & Autor creados

---
+++ src/models/Autor.js
@@ -0,0 +1,6 @@
+class Autor {
+  constructor(nombre, id) {
+    this.nombre = nombre;
+    this.id = id;
+  }
+}
---
+++ src/models/Libro.js
@@ -0,0 +1,8 @@
+class Libro {
+  constructor(titulo, autor, anio, idPortada) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anio = anio;
+    this.idPortada = idPortada;
+  }
+}
