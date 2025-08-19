bac83d8f35e7171ff71af6c317c9e0b2f9ccccfe
ikerglopez <ikergarcialopez01@gmail.com>
2025-08-19 13:21:43 +0200

    Commit grande para cambiar nombre de la rama

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -1,6 +1,10 @@
-class Autor {
-  constructor(nombre, id) {
-    this.nombre = nombre;
-    this.id = id;
+export class Autor {
+  constructor(data) {
+    // Información básica
+    this.key = data.key; // Identificador único del autor (/authors/OL123A)
+    this.name = data.name;
+    this.personalName = data.personal_name || '';
+    this.fullerName = data.fuller_name || '';
   }
+
 }
--- src/models/Libro.js
+++ src/models/Libro.js
@@ -1,8 +1,28 @@
-class Libro {
-  constructor(titulo, autor, anio, idPortada) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.anio = anio;
-    this.idPortada = idPortada;
+export class Libro {
+  constructor(data) {
+    // Información básica
+    this.key = data.key; // Identificador único del libro
+    this.title = data.title;
+    this.authors = data.authors || []; // Array de objetos autor
+    this.authorNames = data.author_name || [];
+    
+    // Fechas y ediciones
+    this.firstPublishYear = data.first_publish_year;
+    this.publishDate = data.publish_date;
+    this.publishYear = data.publish_year;
+    
+    // Identificadores
+    this.isbn = data.isbn || [];
+   
+    // Información adicional
+    this.subjects = data.subject || [];
+    this.publishers = data.publisher || [];
+    
+    // Metadatos
+    this.editionCount = data.edition_count;
+    
+    // Ratings (si disponible)
+    this.ratingsAverage = data.ratings_average;
+    this.ratingsCount = data.ratings_count;
   }
 }
